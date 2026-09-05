4c118b44c05fbe79a537ea75ad526d038da9393a
Gaetan Pawlowski <gaetan@gaetinux.com>
2026-09-05 16:48:51 +0200

    refactor(virtualization): extract libvirt configuration

--- hosts/odin/configuration.nix
+++ hosts/odin/configuration.nix
@@ -8,6 +8,7 @@
     ../../modules/sway.nix
     ../../modules/nvidia.nix
     ../../modules/audio.nix
+    ../../modules/virtualization.nix
   ];
 
   # Boot
@@ -19,14 +20,6 @@
 
   # Gaming
   programs.steam.enable = true;
-
-  # Virtualization
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    dnsmasq
-  ];
 
   # Keep this value at the NixOS version used for the initial installation.
   system.stateVersion = "26.05";
