68a74a98e66eda6515a6154bafe231c63535d163
Gaetan Pawlowski <gaetan@gaetinux.com>
2026-09-05 15:51:02 +0200

    feat: add initial NixOS flake configuration

---
+++ configuration.nix
@@ -0,0 +1,183 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  networking.hostName = "nixos"; 
+  networking.networkmanager.enable = true;
+  services.tailscale.enable = true;
+
+  time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # X11.
+  #services.xserver.enable = true;
+
+  # Enables Gnome Keyring to store secrets for applications. 
+  services.gnome.gnome-keyring.enable = true;
+
+  # Enable Sway.
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraOptions = [ "--unsupported-gpu" ];
+  };
+
+  # Greeter.
+  services.greetd = {                                                      
+    enable = true;                                                         
+    settings = {                                                           
+      default_session = {                                                  
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
+        user = "greeter";                                                  
+      };                                                                   
+    };                                                                     
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Environment = [
+      "GBM_BACKEND=nvidia-drm"
+      "__GLX_VENDOR_LIBRARY_NAME=nvidia"
+      "WLR_NO_HARDWARE_CURSORS=1"
+      "NIXOS_OZONE_WL=1"
+      "MOZ_ENABLE_WAYLAND=1"
+    ];
+  };
+
+
+  # X11 keymap.
+  services.xserver.xkb = {
+    layout = "fr";
+  };
+
+  # Console keymap.
+  console.keyMap = "fr";
+
+  # Nvidia.
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true; 
+    nvidiaSettings = true;
+    #package = config.boot.kernelPackages.nvidiaPackages.stable;
+    powerManagement.enable = false; 
+  };
+
+  # environment.sessionVariables = {
+  #   NIXOS_OZONE_WL = "1"; 
+  #   GBM_BACKEND = "nvidia-drm";
+  #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  #   WLR_NO_HARDWARE_CURSORS = "1"; 
+  # };
+
+  # CUPS.
+  services.printing.enable = true;
+
+  # Pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # User account.
+  users.users."gaetinux" = {
+    isNormalUser = true;
+    description = "Gaetan Pawlowski";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    packages = with pkgs; [
+      # lazyvim
+      fzf
+      ripgrep
+      fd
+      gcc
+
+      git
+      vscodium
+      neovim
+      keepassxc
+      openfortivpn
+      remmina
+    ];
+  };
+
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+  programs.firefox.enable = true;
+  programs.steam.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wl-clipboard # Copy/Paste functionality.
+    mako # Notification utility.
+    wofi # Applications launcher.
+    pavucontrol # Volume.
+    brightnessctl
+    waybar
+
+    htop
+    dnsmasq
+    papirus-icon-theme
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
