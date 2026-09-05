70b3fe51ba6acdda8cbb84fbd50c64f04adfb961
Gaetan Pawlowski <gaetan@gaetinux.com>
2026-09-05 22:39:37 +0200

    feat(home-manager): add Home Manager flake input

--- flake.nix
+++ flake.nix
@@ -3,15 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.odin = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+      system = "x86_64-linux";
 
-        modules = [
-            ./hosts/odin/configuration.nix
-        ];
+      modules = [
+        ./hosts/odin/configuration.nix
+        home-manager.nixosModules.home-manager
+      ];
     };
   };
 }
