7c7382179c6a918476f915cb50af4d194c2f9cd2
Gaetan Pawlowski <gaetan@gaetinux.com>
2026-09-05 16:46:02 +0200

    refactor(common): move shared desktop packages

--- modules/common.nix
+++ modules/common.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 
 {
+  # Localization
   time.timeZone = "Europe/Paris";
 
   i18n.defaultLocale = "fr_FR.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
@@ -17,17 +17,34 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  services.xserver.xkb = {
-    layout = "fr";
-  };
-
+  services.xserver.xkb.layout = "fr";
   console.keyMap = "fr";
 
+  # Networking
   networking.networkmanager.enable = true;
-
+  networking.firewall.enable = true;
   services.tailscale.enable = true;
 
+  # System
   nixpkgs.config.allowUnfree = true;
-
   services.printing.enable = true;
+
+  # Applications
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+
+  programs.firefox.enable = true;
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
+
+  # Utilities
+  environment.systemPackages = with pkgs; [
+    htop
+    papirus-icon-theme
+  ];
 }
