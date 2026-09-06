be97c021383ff82f2165703f4e1fc72c7bb53e88
Gaetan Pawlowski <gaetan@gaetinux.com>
2026-09-06 11:26:43 +0200

    feat(fuzzel): harmonize launcher theme

--- home/gaetinux/fuzzel.nix
+++ home/gaetinux/fuzzel.nix
@@ -11,17 +11,21 @@
         layer = "overlay";
         width = 40;
         lines = 10;
-
         icon-theme = "Papirus-Dark";
       };
 
       colors = {
-        background = "181818ff";
-        text = "e6e6e6ff";
-        match = "ffffffff";
-        selection = "3a3a3aff";
+        background = "2a2938ff";
+        text = "ffffffff";
+        prompt = "a99bc6ff";
+        input = "ffffffff";
+        match = "a99bc6ff";
+
+        selection = "3a3545ff";
         selection-text = "ffffffff";
-        border = "3a3a3aff";
+        selection-match = "d0c4efff";
+
+        border = "a99bc6ff";
       };
 
       border = {
