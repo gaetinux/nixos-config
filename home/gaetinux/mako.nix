eec4f352afebf4470655a2a5a001469811c3a778
Gaetan Pawlowski <gaetan@gaetinux.com>
2026-09-06 11:25:40 +0200

    feat(mako): harmonize notification theme

--- home/gaetinux/mako.nix
+++ home/gaetinux/mako.nix
@@ -5,15 +5,33 @@
     enable = true;
 
     settings = {
-      font = "FiraCode Nerd Font 11";
-      background-color = "#181818";
-      text-color = "#e6e6e6";
-      border-color = "#3a3a3a";
+      font = "FiraCode Nerd Font 10";
+
+      background-color = "#2a2938";
+      text-color = "#ffffff";
+      border-color = "#a99bc6";
+
       border-size = 2;
       border-radius = 6;
-      default-timeout = 5000;
-      padding = "10";
+
+      width = 350;
+      height = 120;
       margin = "10";
+      padding = "12";
+
+      default-timeout = 5000;
+
+      icons = true;
+      max-icon-size = 48;
+
+      anchor = "top-right";
     };
+
+    extraConfig = ''
+      [urgency=high]
+      background-color=#33262c
+      border-color=#a54242
+      default-timeout=0
+    '';
   };
 }
