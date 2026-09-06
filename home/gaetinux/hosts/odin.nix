0c0068a92f17825687ee4ee687b7d0efc541df54
Gaetan Pawlowski <gaetan@gaetinux.com>
2026-09-06 18:24:12 +0200

    feat(odin): configure mouse acceleration profile

--- home/gaetinux/hosts/odin.nix
+++ home/gaetinux/hosts/odin.nix
@@ -1,9 +1,18 @@
 { ... }:
 
 {
-  wayland.windowManager.sway.config.output = {
-    "DP-1" = {
-      mode = "1920x1080@74.973Hz";
+  wayland.windowManager.sway.config = {
+    output = {
+      "DP-1" = {
+        mode = "1920x1080@74.973Hz";
+      };
+    };
+
+    input = {
+      "5426:70:Razer_Razer_Mamba_Tournament_Edition" = {
+        accel_profile = "flat";
+        pointer_accel = "0";
+      };
     };
   };
 }
